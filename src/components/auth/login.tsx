a4b25de37e3c086a97028d192c7ae30020b34555
rickywid <rickywid@hotmail.com>
2020-06-18 04:07:34 -0400

    add styling login/signup pages

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -1,26 +1,27 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import {
-    Form,
-    Input,
-    Button,
-    Checkbox, 
-  } from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
+  Form,
+  Input,
+  Button,
+  Divider
+} from 'antd';
+import { UserOutlined, LockOutlined, GithubOutlined } from '@ant-design/icons';
 import AuthService from '../../lib/authService';
-
+import '../../styles/login.scss';
+import Logo from  '../../assets/monkey.png';
 
 interface IFormValues {
-    username: string;
-    password:string;
-  }
+  username: string;
+  password: string;
+}
 
 function App() {
 
   const [form] = Form.useForm();
   const [error, setError] = useState<boolean>(false)
 
-  const onFinish = async (values:IFormValues) => {
+  const onFinish = async (values: IFormValues) => {
     const api = new AuthService();
     const form = new FormData();
     const { username, password }: IFormValues = values;
@@ -30,78 +31,81 @@ function App() {
 
     const res = await api.login(form);
 
-    if(res.status === 200) {
+    if (res.status === 200) {
       const user = await res.json();
       localStorage.setItem('userID', user.id);
       window.location.replace('http://localhost:3000/');
+
+      return;
     }
-    
+
     setError(true);
 
   };
 
   return (
-      <div>
-          <strong className="login-title">Log in</strong>
-          <small>{error ? 'Username or password is incorrect': ''}</small>
-          <Form
-            name="normal_login"
-            className="login-form"
-            initialValues={{
-              remember: true,
-            }}
-            onFinish={onFinish as any}
+    <div className="login-wrapper">
+      <div className="login-inner">
+        <img style={{height: '50px', marginBottom: '20px'}} src={Logo} alt="logo"/>
+        <h2>Log in</h2>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish as any}
+        >
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Username!',
+              },
+            ]}
           >
-            <Form.Item
-              label="username"
-              name="username"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your Username!',
-                },
-              ]}
-            >
-              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-            </Form.Item>
-            <Form.Item
-              label="password"
-              name="password"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your Password!',
-                },
-              ]}
-            >
-              <Input
-                prefix={<LockOutlined className="site-form-item-icon" />}
-                type="password"
-                placeholder="Password"
-              />
-            </Form.Item>
-            <Form.Item>
-              <Form.Item name="remember" valuePropName="checked" noStyle>
-                <Checkbox>Remember me</Checkbox>
-              </Form.Item>
-
-              <Link to="/reset-password">
-                Forgot password
-              </Link>
-            </Form.Item>
-
-            <Form.Item>
-              <Button type="primary" htmlType="submit" className="login-form-button">
-                Log in
-              </Button>
-              Or
-              <Link to="/signup">
-                Register
-              </Link>
-            </Form.Item>
-          </Form>
-            <button onClick={() => window.open(`http://localhost:5000/api/auth/github`, '_self')}>Github</button>
+            <Input
+              size="large"
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Username"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Password!',
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+              size="large"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              Log in
+            </Button>
+            <p className="error-msg">{error ? 'Username or password is incorrect' : ''}</p>
+            <small className="helper-link">Don't have an account? <Link to="/signup"><strong>Sign Up</strong></Link></small>
+          </Form.Item>
+        </Form>
+        <Divider />
+        <Button
+          type="primary"
+          icon={<GithubOutlined />}
+          className="github-login-btn"
+          onClick={() => window.open(`http://localhost:5000/api/auth/github`, '_self')}
+        >
+          Log In with Github
+            </Button>
       </div>
+    </div>
   );
 }
 
--- src/components/auth/signup.tsx
+++ src/components/auth/signup.tsx
@@ -1,154 +1,171 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import {
-  Form,
-  Input,
-  Tooltip,
-  Checkbox,
-  Button,
+    Form,
+    Input,
+    Tooltip,
+    Checkbox,
+    Button,
+    Divider
 } from 'antd';
-import { QuestionCircleOutlined } from '@ant-design/icons';
+import { QuestionCircleOutlined, GithubOutlined } from '@ant-design/icons';
 import AuthService from '../../lib/authService';
+import Logo from  '../../assets/monkey.png';
 
 function App() {
 
-  const [form] = Form.useForm();
-  const [displayError, setDisplayError] = useState(false);
+    const [form] = Form.useForm();
+    const [displayError, setDisplayError] = useState(false);
 
-  const onFinish = async (values: any) => {
-    const api = new AuthService();
-    const form = new FormData();
-    const { username, email, password } = values;
+    const onFinish = async (values: any) => {
+        const api = new AuthService();
+        const form = new FormData();
+        const { username, email, password } = values;
 
-    form.append('username', username);
-    form.append('password', password);
-    form.append('email', email);
-    
-    const res = await api.signup(form);
+        form.append('username', username);
+        form.append('password', password);
+        form.append('email', email);
 
-    if(res.status === 200) {
-        const user = await res.json();
-        localStorage.setItem('userID', user.id)
-        window.location.replace("http://localhost:3000");
-    } else {
-        const err = await res.json();
-        setDisplayError(err.message)
-    }
-  };
+        const res = await api.signup(form);
 
-  return (
-      <div>
-          {displayError}
-        <Form
-            form={form}
-            name="register"
-            onFinish={onFinish}
-            layout="vertical"
-            scrollToFirstError
-            >
-            <Form.Item
-            name="email"
-            label="E-mail"
-            rules={[
-                {
-                type: 'email',
-                message: 'The input is not valid E-mail!',
-                },
-                {
-                required: true,
-                message: 'Please input your E-mail!',
-                },
-            ]}
-            >
-            <Input />
-            </Form.Item>
+        if (res.status === 200) {
+            const user = await res.json();
+            localStorage.setItem('userID', user.id)
+            window.location.replace("http://localhost:3000");
+        } else {
+            const err = await res.json();
+            setDisplayError(err.message)
+        }
+    };
 
-            <Form.Item
-            name="username"
-            label={
-                (
-                <span>
-                    Username&nbsp;
-                    <Tooltip title="What do you want others to call you?">
-                    <QuestionCircleOutlined />
-                    </Tooltip>
-                </span>
-                )
-            }
-            rules={[
-                {
-                required: true,
-                message: 'Please input your username!',
-                whitespace: true,
-                },
-            ]}
-            >
-            <Input />
-            </Form.Item>
+    return (
+        <div className="login-wrapper">
+            <div className="login-inner">
+                <img style={{height: '50px', marginBottom: '20px'}} src={Logo} alt="logo"/>
+                <h2>Sign Up</h2>
+                <Form
+                    form={form}
+                    name="register"
+                    onFinish={onFinish}
+                    layout="vertical"
+                    scrollToFirstError
+                >
+                    <Form.Item
+                        name="email"
+                        label="E-mail"
+                        rules={[
+                            {
+                                type: 'email',
+                                message: 'Required',
+                            },
+                            {
+                                required: true,
+                                message: 'Required',
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
 
-            <Form.Item
-            name="password"
-            label="Password"
-            rules={[
-                {
-                required: true,
-                message: 'Please input your password!',
-                },
-            ]}
-            hasFeedback
-            >
-            <Input.Password />
-            </Form.Item>
+                    <Form.Item
+                        name="username"
+                        label={
+                            (
+                                <span>
+                                    Username&nbsp;
+                                    <Tooltip title="What do you want others to call you?">
+                                        <QuestionCircleOutlined />
+                                    </Tooltip>
+                                </span>
+                            )
+                        }
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Required',
+                                whitespace: true,
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
 
-            <Form.Item
-            name="confirm"
-            label="Confirm Password"
-            dependencies={['password']}
-            hasFeedback
-            rules={[
-                {
-                required: true,
-                message: 'Please confirm your password!',
-                },
-                ({ getFieldValue }) => ({
-                validator(rule, value) {
-                    if (!value || getFieldValue('password') === value) {
-                    return Promise.resolve();
-                    }
+                    <Form.Item
+                        name="password"
+                        label="Password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Required',
+                            },
+                        ]}
+                        hasFeedback
+                    >
+                        <Input.Password />
+                    </Form.Item>
 
-                    return Promise.reject(new Error('The two passwords that you entered do not match!'));
-                },
-                }),
-            ]}
-            >
-            <Input.Password />
-            </Form.Item>
+                    <Form.Item
+                        name="confirm"
+                        label="Confirm Password"
+                        dependencies={['password']}
+                        hasFeedback
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Required',
+                            },
+                            ({ getFieldValue }) => ({
+                                validator(rule, value) {
+                                    if (!value || getFieldValue('password') === value) {
+                                        return Promise.resolve();
+                                    }
 
-            <Form.Item
-            name="agreement"
-            valuePropName="checked"
-            rules={[
-                {
-                validator: (_, value) => (value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement'))),
-                },
-            ]}
-            >
-            <Checkbox>
-                I have read and understand the
+                                    return Promise.reject(new Error('Passwords do not match'));
+                                },
+                            }),
+                        ]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="agreement"
+                        valuePropName="checked"
+                        rules={[
+                            {
+                                validator: (_, value) => (value ? Promise.resolve() : Promise.reject(new Error('Required'))),
+                            },
+                        ]}
+                    >
+                        <Checkbox>
+                            I have read and understand the
                 <a href="/terms"> rules</a>
-            </Checkbox>
-            </Form.Item>
-            <Form.Item>
-            <Button type="primary" htmlType="submit" className="form-signup-button">
-                Sign up
+                        </Checkbox>
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" className="login-form-button">
+                            Sign up
+                        </Button>
+                        <p className="error-msg">{displayError}</p>
+                        <small className="helper-link">Already have an account? <Link to="/login"><strong>Log In</strong></Link></small>
+                    </Form.Item>
+                </Form>
+                <Divider />
+        <Button
+          type="primary"
+          icon={<GithubOutlined />}
+          className="github-login-btn"
+          onClick={() => window.open(`http://localhost:5000/api/auth/github`, '_self')}
+        >
+          Sign Up with Github
             </Button>
-            </Form.Item>
-            </Form>
-            <button onClick={() => window.open(`http://localhost:5000/api/auth/github`, '_self')}>Github</button>
-      </div>
-  );
+            </div>
+        </div>
+    );
 }
 
 export default App;
+
 
 
 
