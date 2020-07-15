aa1216f54024fb2dc6f8f7d15dc1c39099008fa1
rickywid <rickywid@hotmail.com>
2020-07-15 03:40:17 -0400

    add success message after project upload

--- src/components/projectUpload.tsx
+++ src/components/projectUpload.tsx
@@ -7,9 +7,10 @@ import {
     Upload,
     Modal,
     Button,
-    Tooltip
+    Tooltip,
+    message
 } from 'antd';
-import { PlusOutlined, QuestionCircleOutlined } from '@ant-design/icons';
+import { PlusOutlined, QuestionCircleOutlined, RocketTwoTone } from '@ant-design/icons';
 import ApiService from '../lib/apiService';
 import history from '../lib/history';
 import Placeholder from '../lib/placeholders';
@@ -17,6 +18,7 @@ import { technologies, tags } from '../lib/const';
 import { siteName } from '../lib/const';
 import UrlValidation from '../lib/urlValidation';
 import '../styles/form.scss';
+import '../styles/global.scss';
 
 interface IFormLayoutChange {
     size: string;
@@ -75,15 +77,15 @@ const ProjectUpload = () => {
     const childrenTech = [];
     const childrenTags = [];
 
-    for(let key in technologies) {
+    for (let key in technologies) {
         childrenTech.push(<Option key={key} value={`${technologies[key].slug}`}>{technologies[key].name}</Option>);
     }
 
-    for(let key in tags) {
+    for (let key in tags) {
         childrenTags.push(<Option key={key} value={`${tags[key].slug}`}>{tags[key].name}</Option>);
     }
 
-    const onFormLayoutChange = ({ size }: IFormLayoutChange) => { 
+    const onFormLayoutChange = ({ size }: IFormLayoutChange) => {
         setComponentSize(size);
     };
 
@@ -137,14 +139,14 @@ const ProjectUpload = () => {
         const techArray: any = [];
         const tagArray: any = [];
 
-        for(let key in technologies) {
-            if(technologiesSelect.includes(technologies[key].slug)) {
+        for (let key in technologies) {
+            if (technologiesSelect.includes(technologies[key].slug)) {
                 techArray.push(key);
             }
         }
 
-        for(let key in tags) {
-            if(tagsSelect.includes(tags[key].slug)) {
+        for (let key in tags) {
+            if (tagsSelect.includes(tags[key].slug)) {
                 tagArray.push(key);
             }
         }
@@ -159,11 +161,16 @@ const ProjectUpload = () => {
         form.append('collaboration', collaboration);
         form.append('screenshots', fileListUpload.length ? fileListUpload[fileListUpload.length - 1] : placeholder.project());
         form.append('user_id', userID);
-        
+
         const res = await api.createProject(form);
-        
+
         if (res.status === 200) {
             history.push('/');
+            message.success({
+                content: 'Congratulations! Your project has been successfully uploaded.',
+                icon: <RocketTwoTone twoToneColor='#00d2dc' />,
+                duration: 5
+              });
         }
     }
 
@@ -184,7 +191,7 @@ const ProjectUpload = () => {
         }).then(data => {
             file.onProgress(e => console.log(e));
             file.onSuccess(e => console.log(e));
-            
+
             setFileListUpload([...fileListUpload, data.secure_url]);
         }).catch((err: Error) => {
             console.log(err)
@@ -199,64 +206,64 @@ const ProjectUpload = () => {
     );
 
     return (
-        <div>
-                        <h1>Share Your Project</h1>
+        <div className="container">
+            <h1>Share Your Project</h1>
 
-<div className="form-wrapper">
-            <Form
+            <div className="form-wrapper">
+                <Form
 
-                layout="vertical"
-                initialValues={{
-                    size: componentSize,
-                    collaboration: false,
-                    technologies: [],
-                    tags: []
-                }}
-                onFinish={handleOnFinish as any}
-                onValuesChange={onFormLayoutChange as any}
-            >
-                <Form.Item
-                    label={<span><strong>Title</strong></span>}
-                    name="name"
-                    rules={[{ required: true, message: 'Required' }]}
+                    layout="vertical"
+                    initialValues={{
+                        size: componentSize,
+                        collaboration: false,
+                        technologies: [],
+                        tags: []
+                    }}
+                    onFinish={handleOnFinish as any}
+                    onValuesChange={onFormLayoutChange as any}
                 >
-                    <Input />
-                </Form.Item>
-                <Form.Item
-                    name="tagline"
-                    rules={[{ required: true, message: 'Required' }]}
-                    label={<span><strong>
-                        Tagline&nbsp;
+                    <Form.Item
+                        label={<span><strong>Title</strong></span>}
+                        name="name"
+                        rules={[{ required: true, message: 'Required' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        name="tagline"
+                        rules={[{ required: true, message: 'Required' }]}
+                        label={<span><strong>
+                            Tagline&nbsp;
                 <Tooltip title="A short description about your project">
-                            <QuestionCircleOutlined />
-                        </Tooltip>
+                                <QuestionCircleOutlined />
+                            </Tooltip>
                         </strong></span>}
-                >
-                    <Input />
-                </Form.Item>
-                <Form.Item
-                    name="description"
-                    label={<span><strong>
-                        Description&nbsp;
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        name="description"
+                        label={<span><strong>
+                            Description&nbsp;
                 <Tooltip title="Share your motivation, tech stack, challenges, experience">
-                            <QuestionCircleOutlined />
-                        </Tooltip>
+                                <QuestionCircleOutlined />
+                            </Tooltip>
                         </strong></span>}
-                    rules={[{ required: true, message: 'Required' }]}
-                >
-                    <Input.TextArea 
-                        placeholder="Tell us about your project..."
-                    />
-                </Form.Item>
-                <Form.Item
-                    label={<span><strong>
-                        Website&nbsp;
+                        rules={[{ required: true, message: 'Required' }]}
+                    >
+                        <Input.TextArea
+                            placeholder="Tell us about your project..."
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        label={<span><strong>
+                            Website&nbsp;
                         <Tooltip title="Url of your website">
-                            <QuestionCircleOutlined />
-                        </Tooltip>
+                                <QuestionCircleOutlined />
+                            </Tooltip>
                         </strong></span>}
-                    name="url"
-                    rules={[{ required: true, message: 'Required' },
+                        name="url"
+                        rules={[{ required: true, message: 'Required' },
                         ({ getFieldValue }) => ({
                             validator(rule, value) {
                                 if (UrlValidation(value)) {
@@ -266,21 +273,21 @@ const ProjectUpload = () => {
                                 return Promise.reject(new Error('Invalid Url'));
                             },
                         }),
-                    ]}
-                >
-                    <Input 
-                        placeholder="https://mysite.com"
-                    />
-                </Form.Item>
-                <Form.Item
-                    label={<span><strong>
-                        Repository Url&nbsp;
+                        ]}
+                    >
+                        <Input
+                            placeholder="https://mysite.com"
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        label={<span><strong>
+                            Repository Url&nbsp;
                         <Tooltip title="Url of your project repository">
-                            <QuestionCircleOutlined />
-                        </Tooltip>
+                                <QuestionCircleOutlined />
+                            </Tooltip>
                         </strong></span>}
-                    name="repourl"
-                    rules={[{ required: false, message: 'Required' },
+                        name="repourl"
+                        rules={[{ required: false, message: 'Required' },
                         ({ getFieldValue }) => ({
                             validator(rule, value) {
                                 if (UrlValidation(value) || value === '') {
@@ -290,79 +297,79 @@ const ProjectUpload = () => {
                                 return Promise.reject(new Error('Invalid Url'));
                             },
                         }),
-                    ]}
-                >
-                    <Input 
-                        placeholder="https://mysite.com"
-                    />
-                </Form.Item>
-                <Form.Item
-                    label={<span><strong>Technologies</strong></span>}
-                    name="technologies"
-                    rules={[{ required: true, message: 'Must select at least one' }]}
-                >
-                    <Select
-                        mode="multiple"
-                        style={{ width: '100%' }}
-                        placeholder="Please select"
-                        onChange={onSelectTechnologyChange as any}
+                        ]}
                     >
-                        {childrenTech}
-                    </Select>
-                </Form.Item>
-                <Form.Item
-                    label={<span><strong>Tags</strong></span>}
-                    name="tags"
-                    rules={[{ required: true, message: 'Must select at least one' }]}
-                >
-                    <Select
-                        mode="multiple"
-                        style={{ width: '100%' }}
-                        placeholder="Please select"
-                        onChange={onSelectTagChange as any}
+                        <Input
+                            placeholder="https://mysite.com"
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        label={<span><strong>Technologies</strong></span>}
+                        name="technologies"
+                        rules={[{ required: true, message: 'Must select at least one' }]}
                     >
-                        {childrenTags}
-                    </Select>
-                </Form.Item>
-                <Form.Item label={<span><strong>
-                    Collaboration&nbsp;
+                        <Select
+                            mode="multiple"
+                            style={{ width: '100%' }}
+                            placeholder="Please select"
+                            onChange={onSelectTechnologyChange as any}
+                        >
+                            {childrenTech}
+                        </Select>
+                    </Form.Item>
+                    <Form.Item
+                        label={<span><strong>Tags</strong></span>}
+                        name="tags"
+                        rules={[{ required: true, message: 'Must select at least one' }]}
+                    >
+                        <Select
+                            mode="multiple"
+                            style={{ width: '100%' }}
+                            placeholder="Please select"
+                            onChange={onSelectTagChange as any}
+                        >
+                            {childrenTags}
+                        </Select>
+                    </Form.Item>
+                    <Form.Item label={<span><strong>
+                        Collaboration&nbsp;
             <Tooltip title="Are you interested in collaborating with other developers?">
-                        <QuestionCircleOutlined />
-                    </Tooltip>
-                </strong></span>}
-                    name="collaboration"
-                >
-                    <Switch checkedChildren="Yes" unCheckedChildren="No" />
-                </Form.Item>
-                <Form.Item label={<span><strong>Add Image</strong></span>}>
-                    <p>Include a screenshot or logo of your application.</p>
-                    <div className="clearfix">
-                        <Upload
-                            listType="picture-card"
-                            fileList={fileList as any[]}
-                            onPreview={handlePreview as any}
-                            onChange={handleUploadChange as any}
-                            onRemove={handleOnRemove as any}
-                            customRequest={customRequest as any}
-                        >
-                            {fileList.length >= 1 ? null : uploadButton}
-                        </Upload>
-                        <Modal
-                            visible={previewVisible}
-                            title={previewTitle}
-                            footer={null}
-                            onCancel={handleCancel}
-                        >
-                            <img alt="example" style={{ width: '100%' }} src={previewImage} />
-                        </Modal>
+                            <QuestionCircleOutlined />
+                        </Tooltip>
+                    </strong></span>}
+                        name="collaboration"
+                    >
+                        <Switch checkedChildren="Yes" unCheckedChildren="No" />
+                    </Form.Item>
+                    <Form.Item label={<span><strong>Add Image</strong></span>}>
+                        <p>Include a screenshot or logo of your application.</p>
+                        <div className="clearfix">
+                            <Upload
+                                listType="picture-card"
+                                fileList={fileList as any[]}
+                                onPreview={handlePreview as any}
+                                onChange={handleUploadChange as any}
+                                onRemove={handleOnRemove as any}
+                                customRequest={customRequest as any}
+                            >
+                                {fileList.length >= 1 ? null : uploadButton}
+                            </Upload>
+                            <Modal
+                                visible={previewVisible}
+                                title={previewTitle}
+                                footer={null}
+                                onCancel={handleCancel}
+                            >
+                                <img alt="example" style={{ width: '100%' }} src={previewImage} />
+                            </Modal>
+                        </div>
+                    </Form.Item>
+                    <div className="form-btn-wrap">
+                        <Button className="upload-btn" type="primary" htmlType="submit">Submit</Button>
                     </div>
-                </Form.Item>
-                <div className="form-btn-wrap">
-                    <Button className="upload-btn" type="primary" htmlType="submit">Submit</Button>
-                </div>
-                
-            </Form>
-        </div>
+
+                </Form>
+            </div>
         </div>
     )
 };
