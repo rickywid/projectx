960748abea192d4ec4e6c935e98daf4117ab3a06
rickywid <rickywid@hotmail.com>
2020-07-26 09:01:07 -0400

    add beta label

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,134 +1,133 @@
-import React from 'react';
-import { Button, Divider, Menu, Dropdown } from 'antd';
+import React from "react";
+import { Button, Divider, Menu, Dropdown } from "antd";
 import { Link } from "react-router-dom";
-import Search from './searchBar'
-import useWindowDimensions from '../hooks/windowSize';
-import AuthService from '../lib/authService';
-import { subscriberUrl } from '../lib/const';
-import '../App.scss';
-import '../styles/navbar.scss';
-import '../styles/mobileNav.scss';
-import { slide as MenuSlide } from 'react-burger-menu'
+import Search from "./searchBar";
+import useWindowDimensions from "../hooks/windowSize";
+import AuthService from "../lib/authService";
+import { subscriberUrl } from "../lib/const";
+import "../App.scss";
+import "../styles/navbar.scss";
+import "../styles/mobileNav.scss";
+import { slide as MenuSlide } from "react-burger-menu";
 
-import Logo from '../assets/logo.png';
+import Logo from "../assets/logo.png";
 
 interface Props {
   user: {
-    username: string,
-    gh_avatar: string
+    username: string;
+    gh_avatar: string;
   };
   isAuthenticated: boolean;
   loading: boolean;
 }
 
-const NavBar = ({
-  user, isAuthenticated, loading
-}: Props) => {
+const NavBar = ({ user, isAuthenticated, loading }: Props) => {
   const api = new AuthService();
   const { height, width } = useWindowDimensions();
-
 
   const onSignout = async () => {
     await api.signout(new FormData());
     window.location.replace(process.env.REACT_APP_HOSTNAME as string);
-
-  }
+  };
 
   const menu = (
     <Menu>
       <li>
         <Link className="nav-user-profile-mobile" to={`/user/${user.username}`}>
           Profile
-          </Link>
+        </Link>
       </li>
       <li>
-        <button className="btn-signout" onClick={onSignout}>Sign Out</button>
+        <button className="btn-signout" onClick={onSignout}>
+          Sign Out
+        </button>
       </li>
     </Menu>
   );
 
-  return loading ? <></> : (
+  return loading ? (
+    <></>
+  ) : (
     <nav>
-      {width > 1110 ?
+      {width > 1110 ? (
         <>
           <div>
             <Link to="/">
               <img className="nav-logo" src={Logo} alt="logo" />
+              <strong>beta</strong>
             </Link>
           </div>
           <Search />
           <ul className="nav-item-desktop">
             <li>
-              <Link to="/tag/technology">
-                Technologies
-                </Link>
+              <Link to="/tag/technology">Technologies</Link>
             </li>
             <li>
-              <Link to="/tag/category">
-                Categories
-                </Link>
+              <Link to="/tag/category">Categories</Link>
             </li>
             {isAuthenticated ? (
               <>
                 <li>
                   <Link to={`/upload`}>
-                    <Button className="upload-btn" type="primary">UPLOAD</Button>
+                    <Button className="upload-btn" type="primary">
+                      UPLOAD
+                    </Button>
                   </Link>
                 </li>
                 <li>
-                  <Dropdown overlay={menu} trigger={['click']}>
-                    <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+                  <Dropdown overlay={menu} trigger={["click"]}>
+                    <a
+                      className="ant-dropdown-link"
+                      onClick={(e) => e.preventDefault()}
+                    >
                       {user.username}
                     </a>
                   </Dropdown>
                 </li>
               </>
-            ) : <>
+            ) : (
+              <>
                 <li>
-                  <Link to={`/login`}>
-                    Log In
-                </Link>
+                  <Link to={`/login`}>Log In</Link>
                 </li>
                 <li>
                   <Link className="signup-btn" to={`/signup`}>
                     Sign Up
-                </Link>
+                  </Link>
                 </li>
               </>
-            }
-
+            )}
           </ul>
-        </> :
+        </>
+      ) : (
         <div>
-          {isAuthenticated ?
+          {isAuthenticated ? (
             <MenuSlide>
               <Link to="/">
                 <img className="nav-logo" src={Logo} alt="logo" />
               </Link>
               <Search />
               <Link to={`/upload`}>
-                <Button className="upload-btn" type="primary">UPLOAD</Button>
+                <Button className="upload-btn" type="primary">
+                  UPLOAD
+                </Button>
               </Link>
               <Link className="user-nav-link" to={`/user/${user.username}`}>
                 {user.username}
               </Link>
-              <Link to={`/tag/technology`}>
-                Technology
-            </Link>
-              <Link to={`/tag/category`}>
-                Category
-            </Link>
-              <Link to={`/guidelines`}>
-                Guidelines
-            </Link>
-              <a href={subscriberUrl} target="__blank">Subscribe</a>
+              <Link to={`/tag/technology`}>Technology</Link>
+              <Link to={`/tag/category`}>Category</Link>
+              <Link to={`/guidelines`}>Guidelines</Link>
+              <a href={subscriberUrl} target="__blank">
+                Subscribe
+              </a>
 
-              <Link to={`/feedback`}>
-                Feedback
-            </Link>
-              <button className="btn-signout" onClick={onSignout}>Sign Out</button>
-            </MenuSlide> :
-
+              <Link to={`/feedback`}>Feedback</Link>
+              <button className="btn-signout" onClick={onSignout}>
+                Sign Out
+              </button>
+            </MenuSlide>
+          ) : (
             <MenuSlide>
               <Link to="/">
                 <img className="nav-logo" src={Logo} alt="logo" />
@@ -136,33 +135,25 @@ const NavBar = ({
               <Search />
               <Divider style={{ margin: 0 }} />
               <Link to={`/upload`}>
-                <Button className="upload-btn" type="primary">UPLOAD</Button>
+                <Button className="upload-btn" type="primary">
+                  UPLOAD
+                </Button>
               </Link>
-              <Link to={`/signup`}>
-                Sign Up
-            </Link>
-              <Link to={`/login`}>
-                Log In
-            </Link>
-              <Link to={`/tag/technology`}>
-                Technology
-            </Link>
-              <Link to={`/tag/category`}>
-                Category
-            </Link>
-              <Link to={`/guidelines`}>
-                Guidelines
-            </Link>
-              <a href={subscriberUrl} target="__blank">Subscribe</a>
-              <Link to={`/feedback`}>
-                Feedback
-            </Link>
+              <Link to={`/signup`}>Sign Up</Link>
+              <Link to={`/login`}>Log In</Link>
+              <Link to={`/tag/technology`}>Technology</Link>
+              <Link to={`/tag/category`}>Category</Link>
+              <Link to={`/guidelines`}>Guidelines</Link>
+              <a href={subscriberUrl} target="__blank">
+                Subscribe
+              </a>
+              <Link to={`/feedback`}>Feedback</Link>
             </MenuSlide>
-          }
+          )}
         </div>
-      }
+      )}
     </nav>
-  )
+  );
 };
 
 export default NavBar;
